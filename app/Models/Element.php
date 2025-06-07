170cf399b2419dcd134f88c4a1f61242220b3989
detossj <jorgerubilarssj@gmail.com>
2025-06-07 19:38:07 -0400

    attributes added

--- app/Models/Element.php
+++ app/Models/Element.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Element extends Model
 {
     //
-
+    protected $fillable = [
+        'title',
+        'description',
+        'classification',
+        'imagen'
+    ];
 }
