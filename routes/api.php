87bb445192c88e170cdc61999f6aaaaee6145f17
detossj <jorgerubilarssj@gmail.com>
2025-06-08 17:15:04 -0400

    profile route changed

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/profile', [AuthController::class, 'profile']);
 
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('elements', ElementController::class);
