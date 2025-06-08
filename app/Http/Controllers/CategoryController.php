5e80f4ece823e5dd76108e373632ac937b8d169f
detossj <jorgerubilarssj@gmail.com>
2025-06-08 13:53:54 -0400

    commets added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,12 +6,21 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+     /**
+     * Se obtienen todas las categorias
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function get() {
 
         $categories = Category::all();
         return response()->json($categories);
     }
 
+    /**
+     * Se obtiene una categoria por su id
+     * @param int id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getById() {
 
         $category = Category::find($id);
@@ -23,12 +32,23 @@ class CategoryController extends Controller
         }
     }
 
+    /**
+     * @bodyParam title string
+     * @bodyParam icon string
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request) {
 
         $category = Category::create($request->all());
         return response()->json($category, 201);
     }
 
+     /**
+     * Se actualiza una categoria
+     * @bodyParam title string
+     * @bodyParam icon string
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, $id) {
 
         $category = Category::find($id);
@@ -41,6 +61,11 @@ class CategoryController extends Controller
         }
     }
 
+    /**
+     * Se elimina una categoria
+     * @param int id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function delete($id) {
 
         $category = Category::find($id);
--- app/Http/Controllers/ElementController.php
+++ app/Http/Controllers/ElementController.php
@@ -6,12 +6,21 @@ use Illuminate\Http\Request;
 
 class ElementController extends Controller
 {
+    /**
+     * Se obtienen todas los elementos
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function get() {
 
         $elements = Element::all();
         return response()->json($elements);
     }
 
+    /**
+     * Se obtiene un elemento por su id
+     * @param int id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getById() {
 
         $element = Element::find($id);
@@ -23,12 +32,29 @@ class ElementController extends Controller
         }
     }
 
+    /**
+     * @bodyParam title string
+     * @bodyParam description string
+     * @bodyParam classification int
+     * @bodyParam image string
+     * @bodyParam category_id int
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request) {
 
         $element = Element::create($request->all());
         return response()->json($element, 201);
     }
 
+    /**
+     * Se actualiza un elemento
+     * @bodyParam title string
+     * @bodyParam description string
+     * @bodyParam classification int
+     * @bodyParam image string
+     * @bodyParam category_id int
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, $id) {
 
         $element = Element::find($id);
@@ -41,6 +67,11 @@ class ElementController extends Controller
         }
     }
 
+     /**
+     * Se elimina un elemento
+     * @param int id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function delete($id) {
 
         $element = Element::find($id);
