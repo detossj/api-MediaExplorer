5754e6ba7dfd198a9eddf7e5db2155e706324437
detossj <jorgerubilarssj@gmail.com>
2025-06-08 12:48:07 -0400

    functions added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,51 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function get() {
+
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function getById() {
+
+        $category = Category::find($id);
+        if($category){
+            return response()->json($category);
+        }
+        else {
+            return response()->json(['message' => 'Category not find'], 404);
+        }
+    }
+
+    public function create(Request $request) {
+
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        $category = Category::find($id);
+        if($category) {
+            $category->update($request->all());
+            return response()->json($category);
+        }
+        else {
+            return response()->json(['message' => 'Category not find'], 404);
+        }
+    }
+
+    public function delete($id) {
+
+        $category = Category::find($id);
+        if($category) {
+            $category->delete();
+            return response()->json(['message' => 'Category deleted']);
+        }
+        else {
+            return response()->json(['message' => 'Category not find'], 404);
+        }
+        
+    }
 }
--- app/Http/Controllers/ElementController.php
+++ app/Http/Controllers/ElementController.php
@@ -6,5 +6,51 @@ use Illuminate\Http\Request;
 
 class ElementController extends Controller
 {
-    //
+    public function get() {
+
+        $elements = Element::all();
+        return response()->json($elements);
+    }
+
+    public function getById() {
+
+        $element = Element::find($id);
+        if($element){
+            return response()->json($element);
+        }
+        else {
+            return response()->json(['message' => 'Element not find'], 404);
+        }
+    }
+
+    public function create(Request $request) {
+
+        $element = Element::create($request->all());
+        return response()->json($element, 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        $element = Element::find($id);
+        if($element) {
+            $element->update($request->all());
+            return response()->json($element);
+        }
+        else {
+            return response()->json(['message' => 'Element not find'], 404);
+        }
+    }
+
+    public function delete($id) {
+
+        $element = Element::find($id);
+        if($element) {
+            $element->delete();
+            return response()->json(['message' => 'Element deleted']);
+        }
+        else {
+            return response()->json(['message' => 'Element not find'], 404);
+        }
+        
+    }
 }
