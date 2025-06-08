c901e9f6c9713ab808da8f286d1747a307e6d0de
detossj <jorgerubilarssj@gmail.com>
2025-06-08 16:23:42 -0400

    routes changed

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
 
-    Route::get('/categories', [CategoryController::class, 'get']);
-    Route::get('/categories/{id}', [CategoryController::class, 'getById']);
-    Route::post('/categories', [CategoryController::class, 'create']);
-    Route::put('/categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
-
-    Route::get('/elements', [ElementController::class, 'get']);
-    Route::get('/elements/{id}', [ElementController::class, 'getById']);
-    Route::post('/elements', [ElementController::class, 'create']);
-    Route::put('/elements/{id}', [ElementController::class, 'update']);
-    Route::delete('/elements/{id}', [ElementController::class, 'delete']);
-
-    
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('elements', ElementController::class);
 
 });
 
