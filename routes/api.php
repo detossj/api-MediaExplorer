694ad704ba8d85a39137bbb35fb8478ab34370c4
detossj <jorgerubilarssj@gmail.com>
2025-06-08 15:30:28 -0400

    routes added

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,29 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ElementController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/categories', [CategoryController::class, 'get']);
-Route::get('/categories/{id}', [CategoryController::class, 'getById']);
-Route::post('/categories', [CategoryController::class, 'create']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/elements', [ElementController::class, 'get']);
-Route::get('/elements/{id}', [ElementController::class, 'getById']);
-Route::post('/elements', [ElementController::class, 'create']);
-Route::put('/elements/{id}', [ElementController::class, 'update']);
-Route::delete('/elements/{id}', [ElementController::class, 'delete']);
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/profile', [AuthController::class, 'profile']);
+
+    Route::get('/categories', [CategoryController::class, 'get']);
+    Route::get('/categories/{id}', [CategoryController::class, 'getById']);
+    Route::post('/categories', [CategoryController::class, 'create']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+
+    Route::get('/elements', [ElementController::class, 'get']);
+    Route::get('/elements/{id}', [ElementController::class, 'getById']);
+    Route::post('/elements', [ElementController::class, 'create']);
+    Route::put('/elements/{id}', [ElementController::class, 'update']);
+    Route::delete('/elements/{id}', [ElementController::class, 'delete']);
+
+
+});
 
 
